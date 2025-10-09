1e36cb6b45ad032542cd5c8d3d0eaf4e2297dc04
aggeB8 <brannmarkdev@gmail.com>
2025-10-09 11:26:07 +0200

    feat: added default bg & text color

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,7 @@ import "./App.css"
 function App() {
     return (
         <>
-            <h1 className="p-4 bg-red-400 ">Hello World</h1>
-            <div className="h-10 w-10 bg-amber-300 rounded-full"></div>
+            <h1>Hello World</h1>
         </>
     )
 }
