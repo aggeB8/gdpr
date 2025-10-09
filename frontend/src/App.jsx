29d0a4019e3c579be305c642cf5335ecb878f65d
aggeB8 <brannmarkdev@gmail.com>
2025-10-09 11:19:13 +0200

    feat: added tailwind to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
-import "./App.css";
+import "./App.css"
 
 function App() {
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  );
+    return (
+        <>
+            <h1 className="p-4 bg-red-400 ">Hello World</h1>
+            <div className="h-10 w-10 bg-amber-300 rounded-full"></div>
+        </>
+    )
 }
 
-export default App;
+export default App
