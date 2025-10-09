619bc39206d8c3e29a36190d6c2dee297cb98395
Leonard Helgesson Ferm <leonard.helgesson@gmail.com>
2025-10-09 12:14:15 +0200

    added login button component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
-import "./App.css"
+import './App.css';
+import LoginButton from './components/LoginButton/loginButton';
+
+const handleClick1 = () => {
+  window.location.href = '/login';
+  console.log('logged in');
+};
+
+const handleClick2 = () => {
+  window.location.href = '/register';
+  console.log('registered');
+};
 
 function App() {
-    return (
-        <>
-            <h1>Hello World</h1>
-        </>
-    )
+  return (
+    <>
+      <LoginButton text={'Login'} handleClick={handleClick1} />
+      <LoginButton text={'Register'} handleClick={handleClick2} />
+    </>
+  );
 }
 
-export default App
+export default App;
